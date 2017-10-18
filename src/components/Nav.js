fb90699c44da3eaa83b6a4d45a6ed7080ae94f89
Darren White <darren.j.white@gmail.com>
2017-10-17 21:23:06 -0500

    fix: API updates

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-  Toolbar, 
+  Toolbar,
   NavLink
 } from 'rebass';
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,13 @@
-const api = "https://localhost:5001";
+const api = "http://localhost:3001";
 
 // Generate a unique token for storing post data on the backend server.
 let token = localStorage.token;
-if (!token) 
+if (!token)
   token = localStorage.token = Math.random().toString(36).substr(-8);
 
 const headers = {
-  Accept: "application/json",
-  Authorization: token
+  'Accept': 'application/json',
+  'Authorization': token
 };
 
 // CATEGORIES API
@@ -23,18 +23,15 @@ export const getCategories = () =>
 
 export const getPostsForCategory = category =>
   fetch(`${api}/${category}/posts`, { headers })
-    .then(res => res.json())
-    .then(data => data.posts);
+    .then(res => res.json());
 
 export const getPosts = () =>
-  fetch(`${api}/posts/`, { headers })
-    .then(res => res.json())
-    .then(data => data.posts);
+  fetch(`${api}/posts`, { headers })
+    .then(res => res.json());
 
 export const getPostById = id =>
   fetch(`${api}/posts/${id}`, { headers })
-    .then(res => res.json())
-    .then(data => data.post);
+    .then(res => res.json());
 
 // -- POST
 
@@ -46,10 +43,9 @@ export const addPost = (post) =>
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(post)
-  }).then(res => res.json())
-    .then(data => data.post);
+  }).then(res => res.json());
 
-export const upvotePost = (id) => 
+export const upvotePost = (id) =>
   fetch(`${api}/posts/${id}`, {
     method: "POST",
     headers: {
@@ -57,10 +53,9 @@ export const upvotePost = (id) =>
       "Content-Type": "application/json"
     },
     body: JSON.stringify({ option: 'upVote'  })
-  }).then(res => res.json())
-  .then(data => data.post);
+  }).then(res => res.json());
 
-export const downVotePost = (id) => 
+export const downVotePost = (id) =>
   fetch(`${api}/posts/${id}`, {
     method: "POST",
     headers: {
@@ -68,8 +63,7 @@ export const downVotePost = (id) =>
       "Content-Type": "application/json"
     },
     body: JSON.stringify({ option: 'downVote'  })
-  }).then(res => res.json())
-  .then(data => data.post);
+  }).then(res => res.json());
 
 // -- PUT
 
@@ -80,114 +74,19 @@ export const updatePost = (post) =>
       ...headers,
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ 
-      title: post.title, 
-      body: post.body 
+    body: JSON.stringify({
+      title: post.title,
+      body: post.body
     })
-  }).then(res => res.json())
-  .then(data => data.post);
+  }).then(res => res.json());
 
 // -- DELETE
 
 export const deletePost = (id) =>
   fetch(`${api}/posts/${id}`, { method: 'DELETE', headers: { headers } })
-    .then(res => res.json())
-    .then(data => data.post);
+    .then(res => res.json());
 
 // COMMENTS API
 // -- GET
 
 // TODO
-
-/*
-    Welcome to the Udacity Readable API!
-
-    Use an Authorization header to work with your own data:
-
-    fetch(url, { headers: { 'Authorization': 'whatever-you-want' }})
-
-    The following endpoints are available:
-
-    GET /categories
-      USAGE: 
-        Get all of the categories available for the app. List is found in categories.js.
-        Feel free to extend this list as you desire.
-    
-    GET /:category/posts
-      USAGE:
-        Get all of the posts for a particular category
-
-    GET /posts
-      USAGE:
-        Get all of the posts. Useful for the main page when no category is selected.
-    
-    POST /posts
-      USAGE:
-        Add a new post
-      
-      PARAMS: 
-        id - UUID should be fine, but any unique id will work
-        timestamp - timestamp in whatever format you like, you can use Date.now() if you like
-        title - String
-        body - String
-        author - String
-        category: Any of the categories listed in categories.js. Feel free to extend this list as you desire.
-
-    GET /posts/:id
-      USAGE:
-        Get the details of a single post
-
-    POST /posts/:id
-      USAGE:
-        Used for voting on a post
-      PARAMS:
-        option - String: Either "upVote" or "downVote"
-        
-    PUT /posts/:id
-      USAGE:
-        Edit the details of an existing post
-      PARAMS:
-        title - String
-        body - String
-
-    DELETE /posts/:id
-      USAGE:
-        Sets the deleted flag for a post to 'true'. 
-        Sets the parentDeleted flag for all child comments to 'true'.
-      
-    GET /posts/:id/comments
-      USAGE:
-        Get all the comments for a single post
-    
-    POST /comments
-      USAGE:
-        Add a comment to a post
-
-      PARAMS:
-        id: Any unique ID. As with posts, UUID is probably the best here.
-        timestamp: timestamp. Get this however you want.
-        body: String
-        author: String
-        parentId: Should match a post id in the database.
-
-    GET /comments/:id
-      USAGE:
-        Get the details for a single comment
-
-    POST /comments/:id
-      USAGE:
-        Used for voting on a comment.
-
-    PUT /comments/:id
-      USAGE:
-        Edit the details of an existing comment
-     
-      PARAMS:
-        timestamp: timestamp. Get this however you want.
-        body: String
-
-    DELETE /comments/:id
-      USAGE:
-        Sets a comment's deleted flag to 'true'
-
-*/
